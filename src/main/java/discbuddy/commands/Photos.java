ec99956ffbfb44b8e88a08a88f0e1ecdb1462977
Jon <25947147+JonrGull@users.noreply.github.com>
2022-05-17 23:15:04 +0900

    Refactor: remove unneeded set/get utils

--- src/main/java/discbuddy/commands/Photos.java
+++ src/main/java/discbuddy/commands/Photos.java
@@ -1,38 +1,14 @@
 package discbuddy.commands;
 
 public class Photos {
-    private String copyright;
-    private String date;
-    private String explanation;
-    private String hdurl;
-    private String media_type;
-    private String service_version;
-    private String title;
-    private String url;
-
-    public String getCopyright() {
-        return copyright;
-    }
-
-    public void setCopyright(String copyright) {
-        this.copyright = copyright;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public String getExplanation() {
-        return explanation;
-    }
-
-    public void setExplanation(String explanation) {
-        this.explanation = explanation;
-    }
+    public String copyright;
+    public String date;
+    public String explanation;
+    public String hdurl;
+    public String media_type;
+    public String service_version;
+    public String title;
+    public String url;
 
     public String getHdurl() {
         return hdurl;
@@ -42,42 +18,4 @@ public class Photos {
         this.hdurl = hdurl;
     }
 
-    public String getMedia_type() {
-        return media_type;
-    }
-
-    public void setMedia_type(String media_type) {
-        this.media_type = media_type;
-    }
-
-    public String getService_version() {
-        return service_version;
-    }
-
-    public void setService_version(String service_version) {
-        this.service_version = service_version;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    @Override
-    public String toString() {
-        return "Photos{" +
-                "hdurl='" + hdurl + '\'' +
-                '}';
-    }
 }
