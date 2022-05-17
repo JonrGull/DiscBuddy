4c08c65432df8090e25cc6fe0cc917f4d18da1d6
Jon <25947147+JonrGull@users.noreply.github.com>
2022-05-17 11:09:34 +0900

    Feat: ping pong test sucessful

--- src/main/java/discbuddy/Main.java
+++ src/main/java/discbuddy/Main.java
@@ -2,19 +2,25 @@ package discbuddy;
 
 import javax.security.auth.login.LoginException;
 
+import discbuddy.commands.pingPong;
 import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 
-public class Bot {
-    public static void main(String[] args) throws LoginException {
+public class Main {
+
+    public static void main(String[] args) throws LoginException, InterruptedException {
 
         Dotenv dotenv = Dotenv.load();
 
         JDA bot = JDABuilder.createDefault(dotenv.get("DISCBUDDY_KEY"))
                 .setActivity(Activity.playing("Virtual Studio Code"))
                 .build();
+
+        bot.addEventListener(new pingPong());
+
+        bot.awaitReady();
 
     }
 }
