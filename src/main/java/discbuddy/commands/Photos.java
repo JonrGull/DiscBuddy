98d10874ced016c4cdad79638067621ae7d36754
Jon <25947147+JonrGull@users.noreply.github.com>
2022-05-18 11:25:52 +0900

    Refactor: change how unneeded properties are ignored

--- src/main/java/discbuddy/commands/Photos.java
+++ src/main/java/discbuddy/commands/Photos.java
@@ -1,14 +1,12 @@
 package discbuddy.commands;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+
 public class Photos {
-    public String copyright;
-    public String date;
-    public String explanation;
-    public String hdurl;
-    public String media_type;
-    public String service_version;
-    public String title;
-    public String url;
+
+    private String hdurl;
 
     public String getHdurl() {
         return hdurl;
